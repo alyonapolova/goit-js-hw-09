3bd948cd5cbb46bc22e823fe78a0b23f38b3f574
Alyona <polova_alyona0907@ukr.net>
2023-07-22 16:43:22 +0300

    03 start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+// const formEl = document.querySelector('form');
+// const inputEl = document.querySelectorAll('input');
+// // console.log(inputEl);
+// let newEl = {};
+// let DELAY;
+// console.log(DELAY);
+// formEl.addEventListener('submit', formComplete);
+
+// function formComplete(event) {
+//   event.preventDefault();
+
+//   inputEl.forEach(element => {
+//     newEl[element.name] = element.value;
+//   });
+//   console.log(newEl);
+// }
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) =>
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, 1000)
+  );
 }
+
+createPromise(5, 1000)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
