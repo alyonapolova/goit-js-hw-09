0c3d3138fec72b748bbee75a760b39bae7274eb8
Alyona <polova_alyona0907@ukr.net>
2023-07-22 20:40:20 +0300

    03 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,37 +1,45 @@
-// const formEl = document.querySelector('form');
-// const inputEl = document.querySelectorAll('input');
-// // console.log(inputEl);
-// let newEl = {};
-// let DELAY;
-// console.log(DELAY);
-// formEl.addEventListener('submit', formComplete);
+import Notiflix from 'notiflix';
 
-// function formComplete(event) {
-//   event.preventDefault();
-
-//   inputEl.forEach(element => {
-//     newEl[element.name] = element.value;
-//   });
-//   console.log(newEl);
-// }
+const formEl = document.querySelector('form');
+const inputEl = document.querySelectorAll('input');
+//console.log(formEl.elements);
 
 function createPromise(position, delay) {
-  return new Promise((resolve, reject) =>
-    setTimeout(() => {
+  return new Promise((resolve, reject) => {
+    const intervalId = setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
         resolve({ position, delay });
       } else {
         reject({ position, delay });
       }
-    }, 1000)
-  );
-}
-
-createPromise(5, 1000)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
   });
+}
+formEl.addEventListener('submit', formComplete);
+
+function formComplete(event) {
+  event.preventDefault();
+  const formElements = event.target;
+
+  let delay = Number(formElements.elements['delay'].value);
+  let step = Number(formElements.elements['step'].value);
+  let amount = Number(formElements.elements['amount'].value);
+  //console.log(typeof delay);
+
+  for (let i = 0; i < amount; i += 1) {
+    //console.log('i:', i);
+    createPromise(i + 1, delay * i + step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+  formEl.reset();
+}
